f7a37ade358ff5011c711a23e527496b3bb77b35
ken.60625 <ken.60625@gmail.com>
2015-10-12 22:22:18 -0500

    working currently

---
+++ RouteDistanceCalculator.Tests/WhenInvoking.cs
@@ -0,0 +1,257 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Thoughtworks_Train;
+
+namespace RouteDistanceCalculator.Tests
+{
+    [TestFixture]
+
+    public class WhenInvoking
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+ 
+        [TestCase("ABC")]
+        [TestCase("AD")]
+        [TestCase("ADC")]
+        [TestCase("ADEBCD")]
+        [TestCase("AED")]
+        public void NoSuchRouteOnEmptyInput(string requestedRoute)
+        {
+            var noExistingRoutes = new Dictionary<string, int>();
+
+            var result = _calculator.Invoke(noExistingRoutes, requestedRoute);
+
+            Assert.That(result, Is.EqualTo("NO SUCH ROUTE"));
+        }
+
+        [TestCase("Z")]
+        [TestCase("C")]
+        public void NoSuchRouteOnTooShortInput(string requestedRoute)
+        {
+            var noExistingRoutes = new Dictionary<string, int>();
+
+            var result = _calculator.Invoke(noExistingRoutes, requestedRoute);
+
+            Assert.That(result, Is.EqualTo("NO SUCH ROUTE"));
+        }
+
+        [TestCase("ABC")]
+        [TestCase("AD")]
+        [TestCase("ADC")]
+        [TestCase("ADEBCD")]
+        [TestCase("AED")]
+        public void OnSingleInputRouteNotMatchingExistingRoute(string requestedRouteThatStartsWithA)
+        {
+            var singleRoute = new Dictionary<string, int>
+            {
+                {"DZ", 29}
+            };
+
+            var result = _calculator.Invoke(singleRoute, requestedRouteThatStartsWithA);
+
+            Assert.That(result, Is.EqualTo("NO SUCH ROUTE"));
+        }
+
+        [TestCase("AD", 7)]
+        [TestCase("BC", 11)]
+        [TestCase("CD", 37)]
+        public void OnSingleInputRouteMatchingExistingRoute(string requestedRoute, int distance)
+        {
+            var source = requestedRoute;
+            var singleRoute = new Dictionary<string, int>
+            {
+                {source, distance}
+            };
+
+            var result = _calculator.Invoke(singleRoute, requestedRoute);
+
+            Assert.That(result, Is.EqualTo(distance.ToString()));
+        }
+
+        [Test]
+        public void OnSingleInputRouteMatchingThreeRouteDestinationDoesNotExist()
+        {
+            const string requestedRoute = "AD";
+            var singleRoute = new Dictionary<string, int>
+            {
+                {"AB", 3 },
+                {"AC", 393 },
+            };
+
+            var result = _calculator.Invoke(singleRoute, requestedRoute);
+
+            Assert.That(result, Is.EqualTo("NO SUCH ROUTE"));
+        }
+
+        [Test]
+        public void OnSingleInputRouteMatchingThreeRouteDestinationChoosesDirectPathIgnoresIndirect()
+        {
+            const string requestedRoute = "AC";
+            var singleRoute = new Dictionary<string, int>
+            {
+                {"AB", 3 },
+                {"BC", 77 },
+                {"AC", 393 },
+            };
+
+            var result = _calculator.Invoke(singleRoute, requestedRoute);
+
+            Assert.That(result, Is.EqualTo("393"));
+        }
+
+        [Test]
+        public void On2LegRoute_2ndLegDoesntMatch_ReturnsNoSuchRoute()
+        {
+            const string requestedRoute = "AC";
+            var singleRoute = new Dictionary<string, int>
+            {
+                {"AB", 3 },
+                {"BZ", 77 },
+                {"BD", 393 },
+            };
+
+            var result = _calculator.Invoke(singleRoute, requestedRoute);
+
+            Assert.That(result, Is.EqualTo("NO SUCH ROUTE"));
+        }
+
+        [Test]
+        public void On2LegRoute_BothLegsMatch_ReturnsCorrectWeight()
+        {
+            const string requestedRoute = "ABC";
+            var singleRoute = new Dictionary<string, int>
+            {
+                {"AB", 3 },
+                {"BZ", 77 },
+                {"BC", 393 },
+            };
+
+            var result = _calculator.Invoke(singleRoute, requestedRoute);
+
+            var one = 1;
+            Assert.That(result, Is.EqualTo("396"));
+        }
+
+        [Test]
+        public void ThoughtTest1()
+        {
+            const string requestedRoute = "ABC";
+            var singleRoute = new Dictionary<string, int>
+            {
+                {"AB", 5 },
+                {"BC", 4 },
+                {"CD", 8 },
+                {"DC", 8 },
+                {"DE", 6 },
+                {"AD", 5 },
+                {"CE", 2 },
+                {"EB", 3 },
+                {"AE", 7 },
+            };
+
+            var result = _calculator.Invoke(singleRoute, requestedRoute);
+
+            Assert.That(result, Is.EqualTo("9"));
+        }
+
+        [Test]
+        public void ThoughtTest2()
+        {
+            const string requestedRoute = "AD";
+            var singleRoute = new Dictionary<string, int>
+            {
+                {"AB", 5 },
+                {"BC", 4 },
+                {"CD", 8 },
+                {"DC", 8 },
+                {"DE", 6 },
+                {"AD", 5 },
+                {"CE", 2 },
+                {"EB", 3 },
+                {"AE", 7 },
+            };
+
+            var result = _calculator.Invoke(singleRoute, requestedRoute);
+
+            Assert.That(result, Is.EqualTo("5"));
+        }
+
+        [Test]
+        public void ThoughtTest3()
+        {
+            const string requestedRoute = "ADC";
+            var singleRoute = new Dictionary<string, int>
+            {
+                {"AB", 5 },
+                {"BC", 4 },
+                {"CD", 8 },
+                {"DC", 8 },
+                {"DE", 6 },
+                {"AD", 5 },
+                {"CE", 2 },
+                {"EB", 3 },
+                {"AE", 7 },
+            };
+
+            var result = _calculator.Invoke(singleRoute, requestedRoute);
+
+            Assert.That(result, Is.EqualTo("13"));
+        }
+
+        [Test]
+        public void ThoughtTest4()
+        {
+            const string requestedRoute = "AEBCD";
+            var singleRoute = new Dictionary<string, int>
+            {
+                {"AB", 5 },
+                {"BC", 4 },
+                {"CD", 8 },
+                {"DC", 8 },
+                {"DE", 6 },
+                {"AD", 5 },
+                {"CE", 2 },
+                {"EB", 3 },
+                {"AE", 7 },
+            };
+
+            var result = _calculator.Invoke(singleRoute, requestedRoute);
+
+            Assert.That(result, Is.EqualTo("22"));
+        }
+
+        [Test]
+        public void ThoughtTest5()
+        {
+            const string requestedRoute = "AED";
+            var singleRoute = new Dictionary<string, int>
+            {
+                {"AB", 5 },
+                {"BC", 4 },
+                {"CD", 8 },
+                {"DC", 8 },
+                {"DE", 6 },
+                {"AD", 5 },
+                {"CE", 2 },
+                {"EB", 3 },
+                {"AE", 7 },
+            };
+
+            var result = _calculator.Invoke(singleRoute, requestedRoute);
+
+            Assert.That(result, Is.EqualTo("NO SUCH ROUTE"));
+        }
+    }
+}
---
+++ Thoughtworks_Train/Calculator.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Thoughtworks_Train
+{
+    public class Calculator
+    {
+        public string Invoke(Dictionary<string, int> existingRoutes, string requestedRoute)
+        {
+            if(!existingRoutes.Any() || requestedRoute.Length < 2)
+                return "NO SUCH ROUTE";
+
+            if(existingRoutes.ContainsKey(requestedRoute) && requestedRoute.Length == 2)
+                return existingRoutes[requestedRoute].ToString();
+
+            return DoDepthFirstSearching(existingRoutes, requestedRoute);
+        }
+
+        private static string DoDepthFirstSearching(Dictionary<string, int> existingRoutes, string requestedRoute)
+        {
+            var possibleLegs = existingRoutes.Where(r => r.Key == requestedRoute.Substring(0, 2));
+            
+            foreach (var legs in possibleLegs)
+            {
+                var weight = 0;
+                if (DoDepthFirstSearchingAux(existingRoutes, ref weight, legs.Key, requestedRoute))
+                {
+                    return weight.ToString();
+                };
+            }
+
+            return "NO SUCH ROUTE";
+        }
+
+        private static bool DoDepthFirstSearchingAux(Dictionary<string, int> existingRoutes, ref int runningWeight, string fromToKey, string requestedRoute)
+        {
+            // Concept: the minute you have entered this function, you have gone from...to.  (See fromToKey.)  So update recordkeeping now.
+            runningWeight += existingRoutes[fromToKey];
+
+            if (YouHaveReachedYourDestination(requestedRoute))
+                return true;
+
+            var nextLocation = requestedRoute.Substring(1);
+            var possibleRoutes = existingRoutes.Where(r => r.Key == nextLocation.Substring(0, 2));
+
+            if (!possibleRoutes.Any())
+                return false;
+
+            foreach (var route in possibleRoutes)
+            {
+                var savedWeight = runningWeight;
+                if (DoDepthFirstSearchingAux(existingRoutes, ref runningWeight, route.Key, nextLocation))
+                {
+                    return true;
+                }
+                runningWeight = savedWeight;
+            }
+
+            return false;
+        }
+
+        private static bool YouHaveReachedYourDestination(string requestedRoute)
+        {
+            return requestedRoute.Length == 2;
+        }
+    }
+}
